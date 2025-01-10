dc08a802afbfc1e0f1e47f99431ad850920aeb1f
AGALMAD <agalmad@g.educaand.es>
2025-01-10 18:11:52 +0100

    nav bar , falta animación del icono

--- frontend/chessandconnect/src/app/components/navbar/navbar.component.ts
+++ frontend/chessandconnect/src/app/components/navbar/navbar.component.ts
@@ -4,8 +4,23 @@ import { Component } from '@angular/core';
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  onToggle(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    const nav = document.getElementById('nav');
+    const icon = document.getElementById('menu-icon');
 
+    if (checkbox.checked) {
+
+      nav?.classList.add('hidden-nav');
+      icon?.classList.add('rotate-icon');
+
+    } else {
+
+      nav?.classList.remove('hidden-nav');
+      icon?.classList.remove('rotate-icon');
+    }
+  }
 }

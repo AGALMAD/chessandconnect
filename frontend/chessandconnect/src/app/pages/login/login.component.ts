8affc12127cf18f9a994da7d196397c2002d17b5
noee1410 <noegrelaud@gmail.com>
2025-01-13 18:54:48 +0100

    starting with the formgroup in the login component

--- frontend/chessandconnect/src/app/pages/login/login.component.ts
+++ frontend/chessandconnect/src/app/pages/login/login.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  myForm: FormGroup;
+
+  constructor(
+    //private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.myForm = this.createForm();
+  }
+
+  private createForm(): FormGroup {
+    return this.formBuilder.group(
+      {
+        name: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      }
+    );
+  }
 
 }

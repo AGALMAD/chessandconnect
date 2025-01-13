912999f39d63aced032538a8eb5ecab42831abf5
AGALMAD <agalmad@g.educaand.es>
2025-01-13 14:07:00 +0100

    base de los repositorios

---
+++ backend/chess4connect/chess4connect/ChessAndConnectContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace chess4connect;
+
+public class ChessAndConnectContext : DbContext
+{
+
+    private const string DATABASE_PATH = "chessAndConnect.db";
+
+}

d20125510045047d7833409d642f38fac5a87435
AGALMAD <agalmad@g.educaand.es>
2025-01-14 08:49:18 +0100

    Unit of work created and changes in program

--- backend/chess4connect/chess4connect/Program.cs
+++ backend/chess4connect/chess4connect/Program.cs
@@ -1,8 +1,38 @@
+using chess4connect;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+
+builder.Services.Configure<Settings>(builder.Configuration.GetSection("Settings"));
+builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<Settings>>().Value);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+// CONFIGURANDO JWT
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+
+            // INDICAMOS LA CLAVE
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +45,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Permite CORS
+app.UseCors();
+
 
 app.UseHttpsRedirection();
 
---
+++ backend/chess4connect/chess4connect/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using chess4connect.Repositories;
+
+namespace chess4connect;
+
+public class UnitOfWork
+{
+    private readonly ChessAndConnectContext _context;
+
+    private GameRepository _gameRepository;
+    private PlayRepository _playRepository;
+    private UserRepository _userRepository;
+
+
+    public GameRepository OrderRepository => _gameRepository ??= new GameRepository(_context);
+    public PlayRepository PlayRepository => _playRepository ??= new PlayRepository(_context);
+    public UserRepository UserRepository => _userRepository ??= new UserRepository(_context);
+
+
+
+    public UnitOfWork(ChessAndConnectContext context)
+    {
+        _context = context;
+    }
+
+    public ChessAndConnectContext Context => _context;
+
+    public async Task<bool> SaveAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+}

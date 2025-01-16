9d909b795f9c085046d0e8a8261a785e17133a45
Alejandro Gálvez Madueño <galvezmaduenoalejandro@gmail.com>
2025-01-16 19:58:54 +0100

    clase generica de mensaje

--- backend/chess4connect/chess4connect/Models/SocketComunication/SocketMessage.cs
+++ backend/chess4connect/chess4connect/Models/SocketComunication/SocketMessage.cs
@@ -4,10 +4,17 @@ namespace chess4connect.Models.SocketComunication;
 
 public class SocketMessage
 {
-    public SocketComunicationType Type { get; set; }
+    public virtual SocketComunicationType Type { get; set; }
 }
 
 public class SocketMessage<T> : SocketMessage
 {
+    public T Data { get; set; }
+}
+
+
+public class GameSocketMessage<T> : SocketMessage
+{
+    public override SocketComunicationType Type => SocketComunicationType.GAME;
     public T Data { get; set; }
 }

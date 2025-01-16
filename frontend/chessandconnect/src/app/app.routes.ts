e8970eda7b7821ceef8a2d3eb7d73dbff4601165
Alejandro Gálvez Madueño <galvezmaduenoalejandro@gmail.com>
2025-01-16 20:29:41 +0100

    componente perfil de usuario

--- frontend/chessandconnect/src/app/app.routes.ts
+++ frontend/chessandconnect/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "log", component: LoginComponent},
-    {path: "reg", component: RegisterComponent}
+    {path: "reg", component: RegisterComponent},
+    {path: "userProfile", component: UserProfileComponent}
 
 ];

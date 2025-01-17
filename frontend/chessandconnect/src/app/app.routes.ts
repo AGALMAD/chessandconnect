8dd76385c9e6024718edee410028fd6064073233
noee1410 <noegrelaud@gmail.com>
2025-01-17 19:11:19 +0100

    login and register maybe finished, guard completed and chess and connect4 components created

--- frontend/chessandconnect/src/app/app.routes.ts
+++ frontend/chessandconnect/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ChessMenuComponent } from './pages/chess-menu/chess-menu.component';
+import { redirectionGuard } from './guards/redirection.guard.component';
+import { Connect4MenuComponent } from './pages/connect4-menu/connect4-menu.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "log", component: LoginComponent},
-    {path: "reg", component: RegisterComponent}
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "chess", component: ChessMenuComponent, canActivate: [redirectionGuard]},
+    {path: "connect4", component: Connect4MenuComponent, canActivate: [redirectionGuard]}
 
 ];

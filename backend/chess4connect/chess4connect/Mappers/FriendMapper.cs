2b14aa73db0c29cfee0088daaa18a0ef9f4c69d1
AGALMAD <agalmad@g.educaand.es>
2025-01-20 13:53:41 +0100

    mapper para los amigos

--- backend/chess4connect/chess4connect/Mappers/FriendMapper.cs
+++ backend/chess4connect/chess4connect/Mappers/FriendMapper.cs
@@ -7,5 +7,48 @@ namespace chess4connect.Mappers;
 
 public class FriendMapper
 {
-    
+    private readonly UnitOfWork _unitOfWork;
+
+    public FriendMapper(UnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public FriendDto ToDto(User friend)
+    {
+        return new FriendDto
+        {
+            Id = friend.Id,
+            UserName = friend.UserName,
+            AvatarImageUrl = friend.AvatarImageUrl,
+            Plays = friend.Plays,
+
+        };
+    }
+
+    public IEnumerable<FriendDto> ToDto(IEnumerable<User> friends)
+    {
+        return friends.Select(ToDto);
+    }
+
+
+    public async Task<User> ToEntity(FriendDto friendDto)
+    {
+        User completeUser = await _unitOfWork.UserRepository.GetByIdAsync(friendDto.Id);
+        return completeUser;
+    }
+
+    public async Task<IEnumerable<User>> ToEntity(IEnumerable<FriendDto> friendsDto)
+    {
+        List<User> users = new List<User>();
+
+        foreach (var friendDto in friendsDto)
+        {
+            users.Add(await ToEntity(friendDto));
+        }
+
+
+        return users;
+
+    }
 }

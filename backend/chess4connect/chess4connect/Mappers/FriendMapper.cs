be43a6579d45187a7b29b676e1a2524c20ea5969
AGALMAD <agalmad@g.educaand.es>
2025-01-20 13:42:07 +0100

    error de mapper

--- backend/chess4connect/chess4connect/Mappers/FriendMapper.cs
+++ backend/chess4connect/chess4connect/Mappers/FriendMapper.cs
@@ -7,48 +7,5 @@ namespace chess4connect.Mappers;
 
 public class FriendMapper
 {
-    public PlayDto ToDto(Play play)
-    {
-        return new PlayDto
-        {
-            Id = play.Id,
-            User = play.User,
-            Opponent = play.Opponent,
-            StartDate = play.StartDate,
-            EndDate = play.EndDate,
-            PlayState = play.PlayState,
-            Game = play.Game,
-
-        };
-    }
-
-    public IEnumerable<PlayDto> ToDto(IEnumerable<Play> plays)
-    {
-        return plays.Select(ToDto);
-    }
-
-
-    public Play ToEntity(PlayDto playDto)
-    {
-        return new Play
-        {
-            Id = playDto.Id,
-            UserId = playDto.User.Id,
-            User = playDto.User,
-            OpponentId = playDto.Opponent.Id,
-            Opponent = playDto.Opponent,
-            StartDate = playDto.StartDate,
-            EndDate = playDto.EndDate,
-            PlayState = playDto.PlayState,
-            GameId = playDto.Game.Id,
-            Game = playDto.Game,
-
-        };
-    }
-
-    public IEnumerable<Play> ToEntity(IEnumerable<PlayDto> playsDto)
-    {
-        return playsDto.Select(ToEntity);
-
-    }
+    
 }
--- backend/chess4connect/chess4connect/Mappers/PlayMapper.cs
+++ backend/chess4connect/chess4connect/Mappers/PlayMapper.cs
@@ -1,5 +1,52 @@
-﻿namespace chess4connect.Mappers;
+﻿using chess4connect.Models.Database.DTOs;
+using chess4connect.Models.Database.Entities;
+
+namespace chess4connect.Mappers;
 
 public class PlayMapper
 {
+    public PlayDto ToDto(Play play)
+    {
+        return new PlayDto
+        {
+            Id = play.Id,
+            User = play.User,
+            Opponent = play.Opponent,
+            StartDate = play.StartDate,
+            EndDate = play.EndDate,
+            PlayState = play.PlayState,
+            Game = play.Game,
+
+        };
+    }
+
+    public IEnumerable<PlayDto> ToDto(IEnumerable<Play> plays)
+    {
+        return plays.Select(ToDto);
+    }
+
+
+    public Play ToEntity(PlayDto playDto)
+    {
+        return new Play
+        {
+            Id = playDto.Id,
+            UserId = playDto.User.Id,
+            User = playDto.User,
+            OpponentId = playDto.Opponent.Id,
+            Opponent = playDto.Opponent,
+            StartDate = playDto.StartDate,
+            EndDate = playDto.EndDate,
+            PlayState = playDto.PlayState,
+            GameId = playDto.Game.Id,
+            Game = playDto.Game,
+
+        };
+    }
+
+    public IEnumerable<Play> ToEntity(IEnumerable<PlayDto> playsDto)
+    {
+        return playsDto.Select(ToEntity);
+
+    }
 }

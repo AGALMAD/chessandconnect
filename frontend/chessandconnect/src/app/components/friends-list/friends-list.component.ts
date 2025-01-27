ae001114b8202d00997ef2e5488165510949e560
Manuel M. <manueljmarin@outlook.com>
2025-01-27 17:19:47 +0100

    friend component

--- frontend/chessandconnect/src/app/components/friends-list/friends-list.component.ts
+++ frontend/chessandconnect/src/app/components/friends-list/friends-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FriendsService } from '../../services/friends.service';
+import { Friend } from '../../models/dto/friend';
 
 @Component({
   selector: 'app-friends-list',
@@ -6,6 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './friends-list.component.html',
   styleUrl: './friends-list.component.css'
 })
-export class FriendsListComponent {
+export class FriendsListComponent implements OnInit {
+
+
+  constructor(
+    private friendService: FriendsService
+  ) {
+  }
+  
+  async ngOnInit(): Promise<void> {
+    await this.getUserFriends();
+  }
+
+  friendList: Friend[]
+
+
+
+  async getUserFriends() {
+    const result = await this.friendService.getFriends();
+    this.friendList = result.data
+  }
+  
 
 }

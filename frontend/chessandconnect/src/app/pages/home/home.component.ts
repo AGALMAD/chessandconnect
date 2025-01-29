801540f607885674a5a92b6b01dd4677f118b845
Alejandro Gálvez Madueño <galvezmaduenoalejandro@gmail.com>
2025-01-29 19:38:19 +0100

    volando

--- frontend/chessandconnect/src/app/pages/home/home.component.ts
+++ frontend/chessandconnect/src/app/pages/home/home.component.ts
@@ -4,11 +4,13 @@ import { ApiService } from '../../services/api.service';
 import { WebsocketService } from '../../services/websocket.service';
 import { Subscription } from 'rxjs';
 import { UserService } from '../../services/user.service';
+import { RouterLink } from '@angular/router';
+import { MenuService } from '../../services/menu.service';
 
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -18,7 +20,8 @@ export class HomeComponent implements OnInit{
   constructor(
     private api : ApiService,
     private webSocketService: WebsocketService,
-    private userService: UserService
+    private userService: UserService,
+    private menuService: MenuService
   ){}
 
   async ngOnInit(): Promise<void> {

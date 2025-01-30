d662d7d62b6d2c3fef956c2497ee970a407c7e3f
Alejandro Gálvez Madueño <galvezmaduenoalejandro@gmail.com>
2025-01-30 19:58:16 +0100

    los dos menús son iguales

--- frontend/chessandconnect/src/app/pages/chess-menu/chess-menu.component.ts
+++ frontend/chessandconnect/src/app/pages/chess-menu/chess-menu.component.ts
@@ -18,20 +18,14 @@ import { GameInvitationComponent } from '../../components/game-invitation/game-i
 })
 export class ChessMenuComponent implements OnInit {
 
-  private navigationSubscription: Subscription;
 
   constructor(
     public menuService: MenuService,
     private api: ApiService,
     private webSocketService: WebsocketService,
-    private dialog: MatDialog,
-    private router: Router
+
   ) {
-    this.navigationSubscription = this.router.events.subscribe(event => {
-      if (event instanceof NavigationStart) {
-        this.dialog.closeAll(); // Cierra todos los modales abiertos
-      }
-    });
+    
   }
 
 
@@ -39,25 +33,7 @@ export class ChessMenuComponent implements OnInit {
     await this.webSocketService.connectRxjs()
   }
 
-  ngOnDestroy() {
-    if (this.navigationSubscription) {
-      this.navigationSubscription.unsubscribe();
-    }
-  }
-
-  openSearchModal() {
-    this.dialog.open(UserListComponent, {
-      width: '400px',
-      data: {}  // Puedes pasar datos si necesitas
-    });
-  }
-
-  openGameInvitationModal() {
-    this.dialog.open(GameInvitationComponent, {
-      width: '400px',
-      data: {}  // Puedes pasar datos si necesitas
-    });
-  }
+  
 
 
 }

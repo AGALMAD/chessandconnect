a6fc2928a47a4e551b83ac4badb25e365f947f14
noee1410 <noegrelaud@gmail.com>
2025-02-10 17:11:31 +0100

    chess board started but not good in front

--- frontend/chessandconnect/src/app/pages/chess/chess.component.ts
+++ frontend/chessandconnect/src/app/pages/chess/chess.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chess',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chess.component.html',
   styleUrl: './chess.component.css'
 })
 export class ChessComponent {
+  letters: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  rows: number[] = [8, 7, 6, 5, 4, 3, 2, 1];
+  cells: string[] = [];
+
+  rowsReverse: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+  lettersReverse: string[] = ['H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
 
 }

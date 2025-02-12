29002d3a4f02db85e0f67147e02fb3ea903ae394
manumuno <manueljmarin@outlook.com>
2025-02-12 09:18:24 +0100

    Pawn piece

--- backend/chess4connect/chess4connect/Models/Games/Chess/Pieces/Pawn.cs
+++ backend/chess4connect/chess4connect/Models/Games/Chess/Pieces/Pawn.cs
@@ -5,6 +5,27 @@ namespace chess4connect.Models.Games.Chess.Pieces
 {
     public class Pawn : BasePiece
     {
+        public bool FirstMove { get; set; } = true;
         public Pawn(bool host, Point position) : base(host, position) { }
+
+        protected List<Point> BasicMovements()
+        {
+
+            int direction = Host ? -1 : 1;
+
+            List<Point> basicMovements = new List<Point>
+            {
+                new Point(direction, 0)
+            };
+            if (FirstMove)
+            {
+                basicMovements.Add(new Point(2 * direction,0));
+            }
+
+            return basicMovements;
+        }
+
+
     }
 }
+

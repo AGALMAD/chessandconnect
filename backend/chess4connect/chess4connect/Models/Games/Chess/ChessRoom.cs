211bb8861f868da3e0d8f3a26d1c4d75bcc4e360
noee1410 <noegrelaud@gmail.com>
2025-02-16 16:23:11 +0100

    pieces with different colors

--- backend/chess4connect/chess4connect/Models/Games/Chess/ChessRoom.cs
+++ backend/chess4connect/chess4connect/Models/Games/Chess/ChessRoom.cs
@@ -8,7 +8,7 @@ namespace chess4connect.Models.Games.Chess.Chess
 
         public ChessGame Game { get; set; }
 
-        public ChessRoom (int player1Id, int player2Id, ChessGame game): base(player1Id, player2Id)
+        public ChessRoom (int player1Id, int? player2Id, ChessGame game): base(player1Id, player2Id)
         {
             Game = game;
         }

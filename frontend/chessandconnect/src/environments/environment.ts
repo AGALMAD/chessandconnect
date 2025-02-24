db0e4177849235653b382f32e6bb8942e652c42f
manumuno <manueljmarin@outlook.com>
2025-02-24 14:35:51 +0100

    production url

--- frontend/chessandconnect/src/environments/environment.ts
+++ frontend/chessandconnect/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     imageUrl: 'https://chessandconnect.runasp.net/UserProfilePicture/',
-    apiUrl: 'https://chessandconnect.runasp.net/'
+    apiUrl: 'https://chessandconnect.runasp.net/',
+    socketUrl: "wss://chessandconnect.runasp.net/socket"
+
 };

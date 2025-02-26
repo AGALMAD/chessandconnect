7b915e216a5bed6f1516fc5823acb9cd9a2754e0
noee1410 <noegrelaud@gmail.com>
2025-02-26 19:33:25 +0100

    profile front almost done

--- frontend/chessandconnect/src/app/pages/user-profile/user-profile.component.ts
+++ frontend/chessandconnect/src/app/pages/user-profile/user-profile.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { User } from '../../models/dto/user';
+import { UserService } from '../../services/user.service';
+import { AuthService } from '../../services/auth.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+  activeTab: string = 'chess';
+  public baseUrl = environment.apiUrl; 
+  
+  constructor(
+      public authService: AuthService
+  ) {}
+
+  cambiarTab(tab: string) {
+    this.activeTab = tab;
+  }
+
 
 }
